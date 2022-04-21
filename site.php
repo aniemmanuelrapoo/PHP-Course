0093a35408faf8df48270b85a0e4ca89f07cd3a4
emmanuel rapoo <aniemmanuelrapo@gmail.com>
2022-04-21 15:34:37 +0100

    learning PHP

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $characherName = "john";
+        $characherAge = 35;
+        
+        echo"There once was a man named $characherName <br>";
+        echo"He was $characherAge years old <br>";
+        $characherName = "rapoo";
+        echo"He really liked the name $characherName <br>";
+        echo"But didn't like being $characherAge <br>";
+    ?>
+</body>
+</html>
